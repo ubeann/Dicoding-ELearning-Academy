b22ded523d6367891c026eeb8b59afa8c3716a50
Ubean <ubeannn@gmail.com>
2022-01-24 11:55:36 +0700

    Prepare Layout

--- app/src/main/java/com/dicoding/e_learningacademy/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/dicoding/e_learningacademy/ui/detail/DetailCourseActivity.kt
@@ -23,21 +23,6 @@ class DetailCourseActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_detail_course)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_detail_course)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 
     companion object {
