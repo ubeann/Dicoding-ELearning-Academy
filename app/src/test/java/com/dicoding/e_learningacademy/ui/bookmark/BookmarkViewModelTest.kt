89cd7ac3ff777c71fcdde982434a69d05c5a174c
Ubean <ubeannn@gmail.com>
2022-01-24 12:54:41 +0700

    Unit Test

---
+++ app/src/test/java/com/dicoding/e_learningacademy/ui/bookmark/BookmarkViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.dicoding.e_learningacademy.ui.bookmark
+
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+
+class BookmarkViewModelTest {
+    private lateinit var viewModel: BookmarkViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = BookmarkViewModel()
+    }
+
+    @Test
+    fun getBookmark() {
+        val courseEntities = viewModel.getBookmarks()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
+    }
+}
